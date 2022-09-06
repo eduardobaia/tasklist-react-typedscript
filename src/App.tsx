138386f6af75f7bd4fb3648d0a61cf5168435f59
Eduardo <eduardo_baia@msn.com>
2022-09-06 16:58:42 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as C from './App.styled'
 import './App.css';
+import { useState } from 'react';
+import { Item } from './types/Item'
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
-function App() {
+
+const App = () =>{
+
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name:"buy bread", done:false},
+    {id: 2, name:"READ", done:false},
+  ]);
+ 
+ const handleAddTask = (taskName:string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length+1,
+      name:taskName,
+      done: false
+    });
+
+    setList(newList);
+ }
+
+
+   // Função feita para tarefinha de casa.
+   const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
+const handleOnDelete =(id:number) => {
+
+
+
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.container>
+      <C.Area>
+        <C.Header>Task List</C.Header>
+
+        <AddArea  onEnter={handleAddTask}/>
+        {list.map((item,index)=> (
+          <div> 
+          <ListItem key={index} item={item}     onChange={handleTaskChange}   />
+          </div>
+        ))}
+      </C.Area>
+    </C.container>
+  )
 }
 
 export default App;
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,27 @@
+import * as C from './styles';
+import { Item } from '../../types/Item';
+
+type Props = {
+    item: Item,
+    onChange: (id: number, done: boolean) => void
+}
+
+export const ListItem = ({ item, onChange }: Props) => {
+
+    const handleClick = () =>{
+        
+        console.log("CLICOUU")
+
+    }
+    return (
+        <C.Container done={item.done}>
+            <input
+                type="checkbox"
+                checked={item.done}
+                onChange={e => onChange(item.id, e.target.checked)}
+            />
+            <label>{item.name}</label>
+            {/* <span className="trash" onClick={handleClick}>🗑️</span> */}
+        </C.Container>
+    );
+}
